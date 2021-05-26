72fe3857de362bb86d8e614c100512f24e8e05a6
endrit <58190531+endritbalaj@users.noreply.github.com>
2021-05-26 14:41:19 +0200

    Adding events

--- app/src/main/java/com/example/mobileapp/MainActivity.java
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button start;
+    Button about;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.start);
+        about = (Button) findViewById(R.id.about);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
+
 }
