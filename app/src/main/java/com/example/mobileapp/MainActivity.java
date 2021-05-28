a3b5ff29498e6c273771fb3cd67a3771039ba1f7
endrit <58190531+endritbalaj@users.noreply.github.com>
2021-05-28 23:16:38 +0200

    Adding Menu to the Main Page

--- app/src/main/java/com/example/mobileapp/MainActivity.java
+++ app/src/main/java/com/example/mobileapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.mobileapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button start;
@@ -40,6 +45,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 is seleced", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 is seleced", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 is seleced", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.subitem1:
+                Toast.makeText(this, "SubItem 1 is seleced", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.subitem2:
+                Toast.makeText(this, "SubItem 2 is seleced", Toast.LENGTH_LONG).show();
+                return true;
+            default:return super.onOptionsItemSelected(item);
 
 
+        }
+    }
 }
