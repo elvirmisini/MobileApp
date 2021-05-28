e8931c6b465e7e22dc4ab2d7d3fd9ee93e8aaffd
endrit <58190531+endritbalaj@users.noreply.github.com>
2021-05-28 15:05:05 +0200

    Adding description to images and making them clickable

--- app/src/main/java/com/example/mobileapp/FitnessActivity.java
+++ app/src/main/java/com/example/mobileapp/FitnessActivity.java
@@ -6,10 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class FitnessActivity extends AppCompatActivity {
     Button start;
     Button about;
+    ImageView gymAchievement;
+    ImageView gymFood;
+    ImageView gymMode;
+    ImageView gymTime;
+
+
 
 
     @Override
@@ -19,6 +26,11 @@ public class FitnessActivity extends AppCompatActivity {
 
         start = (Button) findViewById(R.id.start);
         about = (Button) findViewById(R.id.about);
+        gymAchievement = (ImageView) findViewById(R.id.gymAchievement);
+        gymFood = (ImageView) findViewById(R.id.gymFood);
+        gymMode = (ImageView) findViewById(R.id.gymMode);
+        gymTime = (ImageView) findViewById(R.id.gymTime);
+
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +48,43 @@ public class FitnessActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        gymAchievement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gymFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gymMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        gymTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
