28d8452090313205b846dbe43551b5aa9c8e9e34
endrit <58190531+endritbalaj@users.noreply.github.com>
2021-06-03 19:48:54 +0200

    Making time fragment useful for the users

--- app/src/main/java/com/example/mobileapp/TimeFragment.java
+++ app/src/main/java/com/example/mobileapp/TimeFragment.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Chronometer;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -64,6 +66,65 @@ public class TimeFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_time, container, false);
         Button button = (Button) view.findViewById(R.id.button);
+        Chronometer simpleChronometer = (Chronometer) view.findViewById(R.id.simpleChronometer);
+        Button start = (Button) view.findViewById(R.id.startButton);
+        Button stop = (Button) view.findViewById(R.id.stopButton);
+        Button restart = (Button) view.findViewById(R.id.restartButton);
+        Button setFormat = (Button) view.findViewById(R.id.setFormat);
+        Button clearFormat = (Button) view.findViewById(R.id.clearFormat);
+        start.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                simpleChronometer.start();
+            }
+        });
+
+        // perform click  event on stop button to stop the chronometer
+        stop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                simpleChronometer.stop();
+            }
+        });
+
+        // perform click  event on restart button to set the base time on chronometer
+        restart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                simpleChronometer.setBase(SystemClock.elapsedRealtime());
+            }
+        });
+
+        // perform click  event on set Format button to set the format of chronometer
+        setFormat.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                simpleChronometer.setFormat("Time (%s)");
+            }
+        });
+
+        // perform click  event on clear button to clear the current format of chronmeter as you set through set format
+        clearFormat.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                simpleChronometer.setFormat(null);
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
