9b49a055750417fa06cf7a7640c6e39206e0c8bb
elvirmisini <58117020+elvirmisini@users.noreply.github.com>
2021-06-04 23:23:40 +0200

    Program Workout

---
+++ app/src/main/java/com/example/mobileapp/WorkoutProgram.java
@@ -0,0 +1,14 @@
+package com.example.mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkoutProgram extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_program);
+    }
+}
